2d68b7409c70fecf204c35f5884c28bcf2e8713c
Darri Hólmarsson <darri16@ru.is>
2017-10-13 13:43:17 +0000

    Added a method that handles new lines between numbers.

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -7,8 +7,8 @@ public class Calculator {
 			return 0;
 		}
 		else 
-			if (text.contains(",")) {
-				String numbers [] = text.split(",");
+			if (text.contains(",") || text.contains("\n")) {
+				String numbers[] = text.split(",|\n");
 				return sum(numbers);
 			}
 		return 1;
