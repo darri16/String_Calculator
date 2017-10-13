3cdc945bc0785c7bc4bd1f6a601dfe7c73e78f15
Darri Hólmarsson <darri16@ru.is>
2017-10-13 13:38:52 +0000

    Refactoring of sum, the sum function created as a private function.

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -9,17 +9,21 @@ public class Calculator {
 		else 
 			if (text.contains(",")) {
 				String numbers [] = text.split(",");
-				int total = 0;
-				for (String number : numbers) {
-					total += toInt(number);
-				}
-				return total;
+				return sum(numbers);
 			}
 		return 1;
 	}
 
 	private static int toInt (String number) {
 		return Integer.parseInt(number);
+	}
+
+	private static int sum (String [] numbers) {
+		int total = 0;
+		for (String number : numbers) {
+			total += toInt(number);
+		}
+		return total;
 	}
 }
 
